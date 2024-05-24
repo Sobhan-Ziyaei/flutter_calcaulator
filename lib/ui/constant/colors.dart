a15b345df75314ad383a4f13b7b5f6507b953d57
sobhan <sobhan.ziaee73@gmail.com>
2024-05-24 18:37:58 +0330

    init constant color

---
+++ lib/ui/constant/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const greenColor = Color(0xff38b387);
+const greyColor = Color(0xff93a59e);
+const greyBg = Color(0xff2e3951);
+const greyDarkBg = Color(0xff212b41);
