548f45bb80b81cce7711ef46909b70cc4dfd2738
sobhan <sobhan.ziaee73@gmail.com>
2024-05-24 19:07:32 +0330

    add Row of numbers and operator and add clicable feature

--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -25,11 +25,69 @@ class _HomeScreenState extends State<HomeScreen> {
               flex: 7,
               child: Container(
                 color: greyBg,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    getRow('ac', 'ce', '%', '/'),
+                    getRow('7', '8', '9', '*'),
+                    getRow('4', '5', '6', '-'),
+                    getRow('1', '2', '3', '+'),
+                    getRow('00', '0', '.', '='),
+                  ],
+                ),
               ),
             ),
           ],
         ),
       ),
+    );
+  }
+
+  Widget getRow(String text1, String text2, String text3, String text4) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        TextButton(
+          onPressed: () {},
+          child: Padding(
+            padding: const EdgeInsets.all(3.0),
+            child: Text(
+              text1,
+              style: const TextStyle(fontSize: 26, color: greyColor),
+            ),
+          ),
+        ),
+        TextButton(
+          onPressed: () {},
+          child: Padding(
+            padding: const EdgeInsets.all(3.0),
+            child: Text(
+              text2,
+              style: const TextStyle(fontSize: 26, color: greyColor),
+            ),
+          ),
+        ),
+        TextButton(
+          onPressed: () {},
+          child: Padding(
+            padding: const EdgeInsets.all(3.0),
+            child: Text(
+              text3,
+              style: const TextStyle(fontSize: 26, color: greyColor),
+            ),
+          ),
+        ),
+        TextButton(
+          onPressed: () {},
+          child: Padding(
+            padding: const EdgeInsets.all(3.0),
+            child: Text(
+              text4,
+              style: const TextStyle(fontSize: 26, color: greyColor),
+            ),
+          ),
+        )
+      ],
     );
   }
 }
